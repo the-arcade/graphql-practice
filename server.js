9656836374372d5cd54616943f5d74ba14b2c085
Dillon Jeffers <dillon.jeffers@gmail.com>
2017-03-27 23:06:14 -0400

    hello world express app using GraphQL

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    graphqlHTTP = require("express-graphql"),
+    { buildSchema } = require("graphql"),
+    schema = buildSchema(`
+            type Query {
+                hello: String
+            }
+    `),
+    root = { hello: () => "Hello, World!" },
+    app = express();
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000, () => console.log("Now browse to localhost:4000/graphql"));
